c6042c7803e6f24b48dc168580414c538de4e259
giedrius-tumosa <gie.tumosa@gmail.com>
2023-03-30 16:23:33 +0300

    corrected routing

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/reservation" element={<ReservationPage />} />
+        <Route path="/dine-restaurant-website" element={<HomePage />} />
+        <Route path="/dine-restaurant-website/reservation" element={<ReservationPage />} />
       </Routes>
     </>
   );
